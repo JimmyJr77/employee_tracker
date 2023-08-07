eaa4fc1d38ca68a4d3eb28f267e7504c13f8c43f
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-08-06 22:07:54 -0400

    completed work on schema.sql and seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,35 +1,36 @@
--- This SQL file will define the database schema, 
+-- This SQL file will define the database schema,
 -- including the tables for departments, roles, and employees, with their respective columns and relationships.
 
+DROP DATABASE IF EXISTS management_db;
+CREATE DATABASE management_db;
 
+USE management_db;
 
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL, -- holds department name
+    PRIMARY KEY (id)
+);
 
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
 
-
-* `department`
-
-    * `id`: `INT PRIMARY KEY`
-
-    * `name`: `VARCHAR(30)` to hold department name
-
-* `role`
-
-    * `id`: `INT PRIMARY KEY`
-
-    * `title`: `VARCHAR(30)` to hold role title
-
-    * `salary`: `DECIMAL` to hold role salary
-
-    * `department_id`: `INT` to hold reference to department role belongs to
-
-* `employee`
-
-    * `id`: `INT PRIMARY KEY`
-
-    * `first_name`: `VARCHAR(30)` to hold employee first name
-
-    * `last_name`: `VARCHAR(30)` to hold employee last name
-
-    * `role_id`: `INT` to hold reference to employee role
-
-    * `manager_id`: `INT` to hold reference to another employee that is the manager of the current employee (`null` if the employee has no manager)
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,41 @@
--- This SQL file will contain the initial data that will be inserted into the database when it is created. 
--- This will support with sample data for testing your application.
+USE management_db;
+
+-- Insert sample data into the department table
+INSERT INTO department (name)
+VALUES
+  ('Physics'),
+  ('Literature'),
+  ('History'),
+  ('Mathematics'),
+  ('Chemistry'),
+  ('Art'),
+  ('Biology'),
+  ('Philosophy');
+
+-- Insert sample data into the role table
+INSERT INTO role (title, salary, department_id)
+VALUES
+  ('Physicist', 100000, 1),
+  ('Writer', 80000, 2),
+  ('Historian', 75000, 3),
+  ('Mathematician', 90000, 4),
+  ('Chemist', 85000, 5),
+  ('Artist', 70000, 6),
+  ('Biologist', 95000, 7),
+  ('Philosopher', 85000, 8);
+
+-- Insert sample data into the employee table
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Albert', 'Einstein', 1, NULL),
+  ('William', 'Shakespeare', 2, 1),
+  ('Leonardo', 'da Vinci', 3, 1),
+  ('Ada', 'Lovelace', 4, 2),
+  ('Isaac', 'Newton', 1, NULL),
+  ('Jane', 'Austen', 2, 5),
+  ('Napoleon', 'Bonaparte', 3, 5),
+  ('Archimedes', 'of Syracuse', 4, 6),
+  ('Marie', 'Curie', 1, NULL),
+  ('Pablo', 'Picasso', 6, 9),
+  ('Charles', 'Darwin', 7, 9),
+  ('Socrates', NULL, 8, 10);
