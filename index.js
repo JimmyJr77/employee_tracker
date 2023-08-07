d500d5cd5705f428a9c630b6fc06b559da8ef56c
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-08-06 22:22:16 -0400

    added the index.js file with base inquirer prompts. Follow on prompts are pushed to the queries.js file approprately.

--- index.js
+++ index.js
@@ -2,3 +2,59 @@
 // It will handle the command-line interface using the inquirer package and 
 // call the appropriate functions from the models and queries files based on the user's input.
 
+const inquirer = require('inquirer');
+const { viewAllDepartments, viewAllRoles, viewAllEmployees, addDepartment, addRole, addEmployee, updateEmployeeRole } = require('./lib/queries');
+
+// Function to display the main menu
+function mainMenu() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ],
+    }
+  ]).then((answer) => {
+    switch (answer.action) {
+      case 'View all departments':
+        viewAllDepartments();
+        break;
+      case 'View all roles':
+        viewAllRoles();
+        break;
+      case 'View all employees':
+        viewAllEmployees();
+        break;
+      case 'Add a department':
+        addDepartment();
+        break;
+      case 'Add a role':
+        addRole();
+        break;
+      case 'Add an employee':
+        addEmployee();
+        break;
+      case 'Update an employee role':
+        updateEmployeeRole();
+        break;
+      case 'Exit':
+        console.log('Goodbye!');
+        process.exit(0);
+      default:
+        console.log('Invalid choice. Please try again.');
+        mainMenu();
+    }
+  });
+}
+
+// Start the application
+mainMenu();
